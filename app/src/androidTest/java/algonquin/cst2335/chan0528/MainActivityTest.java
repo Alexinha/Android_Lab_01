6051b6cd57f3b57ec93a07106b49513794a56a16
Alexinha Chang <alychang0216@gmail.com>
2023-06-18 15:08:36 -0400

    added javadoc comments and finished 5 test cases

---
+++ app/src/androidTest/java/algonquin/cst2335/chan0528/MainActivityTest.java
@@ -0,0 +1,153 @@
+package algonquin.cst2335.chan0528;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    /**
+     * Test case to confirm that a password with all requirements is accepted.
+     */
+    @Test
+    public void testConfirmGoodPassword(){
+        ViewInteraction appCompatEditText = onView(withId(R.id.password));
+        appCompatEditText.perform(replaceText("passworD1234#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("Your password meets the requirements.")));
+
+    }
+
+    /**
+     * Test case to find a password missing an uppercase character.
+     */
+    @Test
+    public void testFindMissingUpperCase(){
+        ViewInteraction appCompatEditText = onView(withId(R.id.password));
+        appCompatEditText.perform(replaceText("password1234#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+
+    }
+
+    /**
+     * Test case to find a password missing a lowercase character.
+     */
+    @Test
+    public void testFindMissingLowerCase(){
+        ViewInteraction appCompatEditText = onView(withId(R.id.password));
+        appCompatEditText.perform(replaceText("PAW1234#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+
+    }
+
+    /**
+     * Test case to find a password missing a digit.
+     */
+    @Test
+    public void testFindMissingDigit(){
+        ViewInteraction appCompatEditText = onView(withId(R.id.password));
+        appCompatEditText.perform(replaceText("passworD#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+
+    }
+
+    /**
+     * Test case to find a password missing a special character.
+     */
+    @Test
+    public void testFindMissingSpecialCharacter(){
+        ViewInteraction appCompatEditText = onView(withId(R.id.password));
+        appCompatEditText.perform(replaceText("password1234ABC"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+
+    }
+
+    /**
+     * Test case to check the main activity with a short password.
+     */
+    @Test
+    public void mainActivityTest() {
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.password));
+        appCompatEditText.perform(replaceText("12345"), closeSoftKeyboard());
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
