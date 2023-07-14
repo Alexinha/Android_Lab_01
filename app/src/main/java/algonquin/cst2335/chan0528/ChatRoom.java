4e530cfdde0a30af1ab58b32f0f27146905356ef
Alexinha Chang <alychang0216@gmail.com>
2023-07-14 18:45:47 -0400

    fixed minor bugs about received messages not being saved in database

--- app/src/main/java/algonquin/cst2335/chan0528/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/chan0528/ChatRoom.java
@@ -199,6 +199,16 @@ public class ChatRoom extends AppCompatActivity {
             String timeStamp = sdf.format(new Date());
             // set the boolean value
             boolean isSentButton = false;
+
+            ChatMessage newMsg =  new ChatMessage(newMessage, timeStamp, isSentButton);
+
+            Executor thread1 = Executors.newSingleThreadExecutor();
+            thread1.execute( () -> {
+                newMsg.id = mDAO.insertMessage(newMsg);
+            });
+
+            messages.add(newMsg);
+
             messages.add(new ChatMessage(newMessage, timeStamp, isSentButton));
 
             // tell the adapter which row has to be redrawn
